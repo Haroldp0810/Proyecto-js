0ccdc6b0f73d8040c05f05b7f1434e3b34063343
Haroldp0810 <Harold.p2329@gmail.com>
2022-03-04 22:13:48 -0500

    Subiendo Proyecto

---
+++ MusicNow/MusicNow/src/main/java/com/example/MusicNow/MusicNowApplication.java
@@ -0,0 +1,13 @@
+package com.example.MusicNow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicNowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MusicNowApplication.class, args);
+	}
+
+}
---
+++ MusicNow/MusicNow/src/test/java/com/example/MusicNow/MusicNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MusicNow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicNowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
